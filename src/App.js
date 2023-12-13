db134d03228900e4b2e2f2325eddc93d969f9081
Drifty-Cyber <raphaelfadimu@gmail.com>
2023-12-13 23:12:37 +0100

    chore: implement fetching questions & components breakup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,76 @@
-import DateCounter from "./DateCounter";
+// import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+const initialState = {
+  questions: [],
+
+  // 'loading', 'error', 'ready', 'active', 'finished'
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+
+    default:
+      throw new Error("Action Unknown");
+  }
+}
 
 export default function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const res = await fetch("http://localhost:8000/questions");
+        const data = await res.json();
+        dispatch({ type: "dataReceived", payload: data });
+        // console.log(data);
+      } catch (err) {
+        // console.error(err);
+        dispatch({ type: "dataFailed" });
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  const numQuestions = questions.length;
+
   return (
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
